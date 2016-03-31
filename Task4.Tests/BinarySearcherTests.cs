be0686e3a1fbc692fe9a318f8cd6068ea91c201b
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-03-31 18:12:42 +0300

    add test for binary search

---
+++ Task4.Tests/BinarySearcherTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Task4.Tests
+{
+    [TestFixture]
+    public class BinarySearcherTests
+    {
+        [Test]
+        [TestCase(11,13,145,67,14,89,116,34,11,Result = 0)]
+        public int BinarySearch_Test(int value, params int[] ints)
+        {
+            Array.Sort(ints);
+            return BinarySearcher<int>.BinarySearch(ints, value, null);
+        }
+    }
+}
--- Task4/BinarySearcher.cs
+++ Task4/BinarySearcher.cs
@@ -8,9 +8,13 @@ namespace Task4
 {
     public static class BinarySearcher<T>
     {
-        public static int BinarySearch(IList<T> array,T value,IComparer<T> comparator )
+        public static int BinarySearch(IList<T> array,T value,IComparer<T> comparator = null)
         {
-            if (comparator.Compare(value,array[array.Count]) > 0 || comparator.Compare(value,array[0]) < 0)  return -1;
+            if (comparator == null)
+                comparator = Comparer<T>.Default;
+            if (array == null)
+                throw new ArgumentNullException();
+            if (comparator.Compare(value,array[array.Count-1]) > 0 || comparator.Compare(value,array[0]) < 0)  return -1;
             int leftBorder = 0, rightBorder = array.Count, average;
             while (leftBorder < rightBorder)
             {
