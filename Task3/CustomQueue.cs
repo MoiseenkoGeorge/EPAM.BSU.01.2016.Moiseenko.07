c4ad4e221b3edbe68bb7a3d498b3c1ad6dc62981
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-03-31 14:45:25 +0300

    task3 done

---
+++ Task3/CustomQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class CustomQueue<T> : IEnumerable<T>
+    {
+        private T[] array;
+        public int Count { get; private set; } = 0;
+        public int Capacity { get; private set; } = 10;
+        
+        public CustomQueue()
+        {
+            array = new T[Capacity];
+        }
+
+        public void Enqueue(T element)
+        {
+            if(Count == Capacity)
+                Realloc();
+            array[Count++] = element;
+        }
+
+        public T Dequeue()
+        {
+            T returnValue = array[0];
+            T[] temp = new T[Capacity];
+            for (int i = 1; i < Count; i++)
+                temp[i] = array[i];
+            return returnValue;
+        }
+
+        public T Peek()
+        {
+            if (Count != 0)
+                return array[0];
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        private void Realloc()
+        {
+            Capacity *= 2;
+            T[] temp = new T[Capacity];
+            for (int i = 0; i < Count; i++)
+                temp[i] = array[i];
+            array = temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CustomQueueEnum<T>(array);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Task3/CustomQueueEnum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Task3
+{
+    public class CustomQueueEnum<T> : IEnumerator<T>
+    {
+        private readonly T[] _array;
+        private int pointer = -1;
+        public T Current
+        {
+            get
+            {
+                if (pointer != -1)
+                    return _array[pointer];
+                throw new InvalidOperationException();
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public CustomQueueEnum(T[] array)
+        {
+            _array = array;
+        }
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            if (pointer < _array.Length - 1)
+            {
+                pointer++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            pointer = -1;
+        }
+    }
+}
