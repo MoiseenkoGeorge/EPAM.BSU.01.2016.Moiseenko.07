160e1a5d384de983bb80c032c5ed9054c1a4d96f
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-03-31 13:20:53 +0300

    task 2 done

---
+++ Task2/Fibonachi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Fibonachi : IEnumerable<decimal>
+    {
+        public IEnumerator<decimal> GetEnumerator()
+        {
+            decimal prevValue = 1;
+            decimal currentValue = 1;
+            yield return prevValue;
+            yield return currentValue;
+            while (true)
+            {
+                decimal nextValue = prevValue + currentValue;
+                yield return nextValue;
+                prevValue = currentValue;
+                currentValue = nextValue;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
